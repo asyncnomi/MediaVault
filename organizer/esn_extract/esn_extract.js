a20b284fa71239a23ad42ec892ae280f3100c00c
asyncnomi <41596453+asyncnomi@users.noreply.github.com>
2022-08-25 01:28:52 +0200

    Update esn_extract.js

--- organizer/esn_extract/esn_extract.js
+++ organizer/esn_extract/esn_extract.js
@@ -1,54 +1,55 @@
-module.exports.extract = function (str) {
+module.exports.extract = function(str) {
     return extract(str)
 };
 
-module.exports.clean = function (str) {
+module.exports.clean = function(str) {
     return clean(str)
 };
 
 function extract(exp) {
-	var RegExp = [/[sS]([0-9]+)[ ._x-]*[eE]([0-9]+)/g,
-				        /[eE]([0-9]+)/g,
-				        /([0-9]+)[ ._x-]+([0-9]+)/g,
-				        /([0-9]+)/g]
-	if (RegExp[0].test(exp)) {
-		var res = exp.match(RegExp[0])[0]
-		res = res.match(/([0-9]+)+/g)
-		return {
-			season: parseInt(res[0]),
-			episode: parseInt(res[1])
-		}
-	}
-	if (RegExp[1].test(exp)) {
-		var res = exp.match(RegExp[1])[0]
-		res = res.match(/([0-9]+)+/g)
-		return {
-			season: undefined,
-			episode: parseInt(res[0])
-		}
-	}
-	if (RegExp[2].test(exp)) {
-		var res = exp.match(RegExp[2])[0]
-		res = res.match(/([0-9]+)+/g)
-		return {
-			season: parseInt(res[0]),
-			episode: parseInt(res[1])
-		}
-	}
-	if (RegExp[3].test(exp)) {
-		var res = exp.match(RegExp[3])[0]
-		res = res.match(/([0-9]+)+/g)
-		return {
-			season: undefined,
-			episode: parseInt(res[0])
-		}
-	}
-	return -1
+    var RegExp = [/[sS]([0-9]+)[ ._x-]*[eE]([0-9]+)/g,
+        /[eE]([0-9]+)/g,
+        /([0-9]+)[ ._x-]+([0-9]+)/g,
+        /([0-9]+)/g
+    ]
+    if (RegExp[0].test(exp)) {
+        var res = exp.match(RegExp[0])[0]
+        res = res.match(/([0-9]+)+/g)
+        return {
+            season: parseInt(res[0]),
+            episode: parseInt(res[1])
+        }
+    }
+    if (RegExp[1].test(exp)) {
+        var res = exp.match(RegExp[1])[0]
+        res = res.match(/([0-9]+)+/g)
+        return {
+            season: undefined,
+            episode: parseInt(res[0])
+        }
+    }
+    if (RegExp[2].test(exp)) {
+        var res = exp.match(RegExp[2])[0]
+        res = res.match(/([0-9]+)+/g)
+        return {
+            season: parseInt(res[0]),
+            episode: parseInt(res[1])
+        }
+    }
+    if (RegExp[3].test(exp)) {
+        var res = exp.match(RegExp[3])[0]
+        res = res.match(/([0-9]+)+/g)
+        return {
+            season: undefined,
+            episode: parseInt(res[0])
+        }
+    }
+    return -1
 }
 
 function clean(exp) {
-  var res = exp
-  res = res.replace(/[ _:,;'"{}|()\[\]]/g, "-")
-  res = res.replace(/([-])\1+/g, "-")
-  return res
+    var res = exp
+    res = res.replace(/[ _:,;'"{}|()\[\]]/g, "-")
+    res = res.replace(/([-])\1+/g, "-")
+    return res
 }
